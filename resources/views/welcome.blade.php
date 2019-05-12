8c8ad0a77e97fc8f0f16d99f03389a2b960de840
jp-morales <morales.juan.rodriguez@gmail.com>
2019-05-12 10:20:08 -0500

    cambio de nombre

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Comictlan</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Comictlan
                 </div>
 
                 <div class="links">
